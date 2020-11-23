47f08d81eaebbc74202f292b0ceed3de97344b38
Nahid2194 <nahidjc80@gmail.com>
2020-11-23 23:19:50 +0600

    show Ingredients according to state

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Ingredient from '../Ingredient/Ingredient';
+import './Burger.css';
+const Burger = props => {
+    let ingredientArr = props.ingredients.map(item => {
+        let amountArr = [...Array(item.amount).keys()];
+        return amountArr.map(_ => {
+            return <Ingredient type={item.type} key={Math.random()} />
+        })
+    })
+        .reduce((arr, element) => {
+            return arr.concat(element);
+        }, []);
 
-const Burger = () => {
+    if (ingredientArr.length === 0) {
+        ingredientArr = <p>Please add some ingreadients!</p>
+    }
     return (
-        <div>
+        <div className="Burger">
             <Ingredient type="bread-top"></Ingredient>
-            <Ingredient type="salad"></Ingredient>
-            <Ingredient type="cheese"></Ingredient>
-            <Ingredient type="meat"></Ingredient>
+            {ingredientArr}
             <Ingredient type="bread-bottom"></Ingredient>
         </div>
     );
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import Burger from './Burger/Burger';
 
 export default class BurgerBuilder extends Component {
-
+    state = {
+        ingredients: [
+            { type: 'salad', amount: 1 },
+            { type: 'cheese', amount: 1 },
+            { type: 'meat', amount: 2 }
+        ]
+    }
     render() {
         return (
             <div>
-                <Burger></Burger>
+                <Burger ingredients={this.state.ingredients}></Burger>
             </div>
         );
     }
