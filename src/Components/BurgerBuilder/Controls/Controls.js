49c488bb23a66a04c237391ea3539e72cae772a7
Nahid2194 <nahidjc80@gmail.com>
2020-11-24 21:00:46 +0600

    update add and remove ingredients

--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -11,7 +11,7 @@ const BuildControl = props => {
     return (
         <div className="d-flex">
             <div className="mr-auto ml-5" style={{ fontWeight: "bold", fontSize: "1.2rem" }}>{props.label}</div>
-            <button className="btn btn-danger btn-sm m-1" onClick={props.}>Less</button>
+            <button className="btn btn-danger btn-sm m-1" onClick={props.removed}>Less</button>
             <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
         </div>
     )
