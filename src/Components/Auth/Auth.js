49886b89f936f8187a7d742fe324dff1303725ca
Nahid2194 <nahidjc80@gmail.com>
2020-12-05 21:34:37 +0600

    adding validation

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -19,34 +19,67 @@ class Auth extends Component {
                             console.log(values);
                         }
                     }
+                    validate={(values) => {
+                        const errors = {};
+
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid email address';
+                        }
+
+                        if (!values.password) {
+                            errors.password = 'Required';
+                        } else if (values.password.length < 4) {
+                            errors.password = 'Must be atleast 4 characters!';
+                        }
+
+                        if (!values.passwordConfirm) {
+                            errors.passwordConfirm = 'Required';
+                        } else if (values.password !== values.passwordConfirm) {
+                            errors.passwordConfirm = 'Password field does no match!';
+                        }
+                        //console.log("Errors:", errors)
+                        return errors;
+                    }}
 
                 >
                     {
-                        ({ values, handleChange, handleSubmit }) => (<div>
-                            <form onSubmit={handleSubmit}>
-                                <input name="email"
-                                    placeholder="Enter Your Email"
-                                    className="form-control"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                />
-                                <br />
-                                <input name="password"
-                                    placeholder="Password"
-                                    className="form-control"
-                                    value={values.password}
-                                    onChange={handleChange}
-                                />
-                                <br />
-                                <input name="passwordConfirm"
-                                    placeholder="Confirm Password"
-                                    className="form-control"
-                                    value={values.passwordConfirm}
-                                    onChange={handleChange}
-                                />
-                                <br />
-                                <button type className="btn btn-success">Sign Up</button>
-                            </form></div>)
+                        ({ values, handleChange, handleSubmit, errors }) => (
+                            <div
+                                style={{
+                                    border: "1px grey solid",
+                                    padding: "15px",
+                                    borderRadius: "7px",
+                                }}>
+                                <form onSubmit={handleSubmit}>
+                                    <input name="email"
+                                        placeholder="Enter Your Email"
+                                        className="form-control"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                    />
+                                    <span style={{ color: "red" }}>{errors.email}</span>
+                                    <br />
+
+                                    <input name="password"
+                                        placeholder="Password"
+                                        className="form-control"
+                                        value={values.password}
+                                        onChange={handleChange}
+                                    />
+                                    <span style={{ color: "red" }}>{errors.password}</span>
+                                    <br />
+                                    <input name="passwordConfirm"
+                                        placeholder="Confirm Password"
+                                        className="form-control"
+                                        value={values.passwordConfirm}
+                                        onChange={handleChange}
+                                    />
+                                    <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
+                                    <br />
+                                    <button type className="btn btn-success">Sign Up</button>
+                                </form></div>)
                     }
                 </Formik>
             </div>
