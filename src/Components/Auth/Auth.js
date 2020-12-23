a399968bfe2719a1009844d378556dc5294bbfe3
Nahid2194 <nahidjc80@gmail.com>
2020-12-23 20:15:12 +0600

    Switching Between Sign Up and Login

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -2,6 +2,12 @@ import { Formik } from 'formik';
 import React, { Component } from 'react';
 
 class Auth extends Component {
+    state = {
+        mode: "Sign Up"
+    }
+    switchModeHandler = () => {
+        this.setState({ mode: this.state.mode === "Sign Up" ? "Login" : "Sign Up" })
+    }
     render() {
         return (
             <div>
@@ -34,11 +40,15 @@ class Auth extends Component {
                             errors.password = 'Must be atleast 4 characters!';
                         }
 
-                        if (!values.passwordConfirm) {
-                            errors.passwordConfirm = 'Required';
-                        } else if (values.password !== values.passwordConfirm) {
-                            errors.passwordConfirm = 'Password field does no match!';
+                        if (this.state.mode === "Sign Up") {
+
+                            if (!values.passwordConfirm) {
+                                errors.passwordConfirm = 'Required';
+                            } else if (values.password !== values.passwordConfirm) {
+                                errors.passwordConfirm = 'Password field does no match!';
+                            }
                         }
+
                         //console.log("Errors:", errors)
                         return errors;
                     }}
@@ -52,6 +62,13 @@ class Auth extends Component {
                                     padding: "15px",
                                     borderRadius: "7px",
                                 }}>
+                                <button style={{
+                                    width: "100%",
+                                    backgroundColor: "#D70F64",
+                                    color: "white",
+                                }} className="btn btn-lg" onClick={this.switchModeHandler}>Switch to {this.state.mode === "Sign Up" ? "Login" : "Sign Up"} </button>
+                                <br />
+                                <br />
                                 <form onSubmit={handleSubmit}>
                                     <input name="email"
                                         placeholder="Enter Your Email"
@@ -70,15 +87,18 @@ class Auth extends Component {
                                     />
                                     <span style={{ color: "red" }}>{errors.password}</span>
                                     <br />
-                                    <input name="passwordConfirm"
-                                        placeholder="Confirm Password"
-                                        className="form-control"
-                                        value={values.passwordConfirm}
-                                        onChange={handleChange}
-                                    />
-                                    <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
-                                    <br />
-                                    <button type className="btn btn-success">Sign Up</button>
+                                    {this.state.mode === "Sign Up" ? <div>
+                                        <input name="passwordConfirm"
+                                            placeholder="Confirm Password"
+                                            className="form-control"
+                                            value={values.passwordConfirm}
+                                            onChange={handleChange}
+                                        />
+                                        <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
+                                        <br />
+                                    </div> : null}
+
+                                    <button type className="btn btn-success">{this.state.mode === "Sign Up" ? "Sign Up" : "Login"}</button>
                                 </form></div>)
                     }
                 </Formik>
